88fa6d642f25fa46bf13201d3136915048be6936
Giles Roadnight <giles.roadnight@gmail.com>
2022-04-30 08:48:03 +0100

    Add progress object type predicates

--- src/helpers/type-predicates.ts
+++ src/helpers/type-predicates.ts
@@ -1,4 +1,4 @@
-import { ICopyStats, IFileStats, ITotals } from '../contracts';
+import { ICopyStats, IFilesProgress, IFileStats, IStreamProgress, ITotals } from '../contracts';
 
 export function isIFileStats(value: unknown): value is IFileStats {
     const stats = value as IFileStats;
@@ -17,4 +17,37 @@ export function isICopyStats(value: unknown): value is ICopyStats {
 
 export function isDefined<T>(value: T | null | undefined): value is T {
     return value != null;
+}
+
+export function isIStreamProgress(value: unknown): value is IStreamProgress {
+    const streamProgress = value as IStreamProgress;
+    return (
+        streamProgress.type === 'fileStreamProgress' &&
+        typeof streamProgress.stats === 'object' &&
+        new Array<keyof IStreamProgress>(
+            'percentage',
+            'transferred',
+            'length',
+            'remaining',
+            'eta',
+            'runtime',
+            'delta',
+            'speed'
+        ).every((key) => typeof streamProgress[key] === 'number')
+    );
+}
+
+export function isIFilesProgress(value: unknown): value is IFilesProgress {
+    const fileProgress = value as IFilesProgress;
+    return (
+        fileProgress.type === 'filesProgress' &&
+        new Array<keyof IFilesProgress>(
+            'totalFiles',
+            'completedFiles',
+            'totalBytes',
+            'completedBytes',
+            'started',
+            'elapsed'
+        ).every((key) => typeof fileProgress[key] === 'number')
+    );
 }
